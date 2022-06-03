d8d902d8a2304cdf6bba443fbfc7a701ac98e950
James Mwendwa <mwendwajames32@gmail.com>
2022-06-03 23:19:21 +0300

    JWT-Auth

--- server.js
+++ server.js
@@ -1,4 +1,39 @@
+//load .env variables
+require('dotenv').config();
+
 const express =  require('express');
 const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const posts = [
+    {
+        username: 'Jason Tatum',
+        title: 'Journey to the NBA finals'
+    },
+       {
+        username: 'Stephen Curry',
+        title: 'Splash brothers'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+});
+
+app.post('/login', (req, res) => {
+    //authenticate the user
+
+    //get username
+    const username = res.body.username
+    const user = { name:username }
+
+    //serialize using jwt
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+
+});
 
 app.listen(3000, 'localhost');
