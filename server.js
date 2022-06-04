ec4144ef70e986d67495bc9ff73882dafc63517a
James Mwendwa <mwendwajames32@gmail.com>
2022-06-04 23:27:34 +0300

    JWT Auth

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//load .env variables
+// load .env variables
 require('dotenv').config();
 
 const express =  require('express');
@@ -10,30 +10,45 @@ app.use(express.json());
 
 const posts = [
     {
-        username: 'Jason Tatum',
+        username: 'Jason',
         title: 'Journey to the NBA finals'
     },
        {
-        username: 'Stephen Curry',
+        username: 'Curry',
         title: 'Splash brothers'
     }
 ]
 
 app.get('/posts', (req, res) => {
-    res.json(posts)
+    res.json(posts.filter(post => post.username === req.user.name))
 });
 
-app.post('/login', (req, res) => {
-    //authenticate the user
+app.post('/login', authenticateToken, (req, res) => {
+    // authenticate the user
 
-    //get username
+    // get username
     const username = res.body.username
     const user = { name:username }
 
-    //serialize using jwt
+    // serialize using jwt
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
     res.json({ accessToken: accessToken });
 
 });
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    
+    if(token === null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+
+}
 
 app.listen(3000, 'localhost');
